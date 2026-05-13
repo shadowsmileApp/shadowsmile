78208aa6cbd507d63c3c9b460aef1f1098ea7974
ShadowSmile <shadowsmile.app@gmail.com>
2026-05-13 14:33:35 -0700

    Fix empty page modules

---
+++ app/messages/page.tsx
@@ -0,0 +1,18 @@
+export default function MessagesPage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#0A0A0F",
+        color: "#fff",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 24,
+        fontWeight: 700,
+      }}
+    >
+      Messages coming soon
+    </main>
+  );
+}
